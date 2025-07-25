a561b113eb989d8f97d08a4d5da08d28ece7f2f2
ilegitt <ilegithub@gmail.com>
2025-07-25 12:50:36 +0200

    Create backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
